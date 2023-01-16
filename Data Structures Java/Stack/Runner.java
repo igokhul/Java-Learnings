9c3fd28f03cf0678bff4ca8875d2b92d5ca1d135
igokhul <gokhul@skiff.com>
2023-01-16 10:05:30 +0530

    Stack using linkedlist

---
+++ Data Structures Java/Stack/Runner.java
@@ -0,0 +1,20 @@
+
+public class Runner {
+    public static void main(String[] args) {
+        StackImpl stack = new StackImpl();
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+        stack.push(9);
+
+        stack.display();
+
+        stack.pop();
+
+        stack.peek();
+        stack.display();
+
+        System.out.println(stack.isEmpty());
+    }
+}
---
+++ Data Structures Java/Stack/StackImpl.java
@@ -0,0 +1,84 @@
+// Implementing stack using linked list
+class StackLinkedList {
+    int data;
+    StackLinkedList next;
+}
+
+public class StackImpl {
+
+    StackLinkedList head;
+
+    public void push(int data) {
+        StackLinkedList node = new StackLinkedList();
+        node.data = data;
+        node.next = null;
+
+        if (head == null) {
+            head = node;
+            System.out.println("New stack is created");
+        } else {
+            StackLinkedList n = head;
+
+            while (n.next != null) {
+                n = n.next;
+            }
+            n.next = node;
+        }
+
+    }
+
+    public void pop() {
+
+        if (head == null) {
+            System.out.println("Nothing to pop");
+        } else {
+            StackLinkedList n = head;
+            int size = sizeOf();
+            for (int i = 1; i < size - 1; i++) {
+                n = n.next;
+            }
+            n.next = null;
+        }
+    }
+
+    public void peek() {
+
+        StackLinkedList n = head;
+        while (n.next != null) {
+            n = n.next;
+        }
+        System.out.println(n.data + " is at peek");
+    }
+
+    public boolean isEmpty() {
+
+        if (head == null)
+            return true;
+        else
+            return false;
+    }
+
+    public int sizeOf() {
+
+        int size = 0;
+        if (head == null) {
+            return 0;
+        } else {
+            StackLinkedList n = head;
+            while (n.next != null) {
+                n = n.next;
+                size++;
+            }
+        }
+        return size + 1;
+    }
+
+    public void display() {
+        StackLinkedList n = head;
+        while (n.next != null) {
+            System.out.println(n.data);
+            n = n.next;
+        }
+        System.out.println(n.data);
+    }
+}
